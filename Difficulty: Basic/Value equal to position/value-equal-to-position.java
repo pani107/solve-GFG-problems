6a1838e0d34d113d70301bf6343b2554f2894f7d
Pranav Wadkar <pranavwadkar2335@gmail.com>
2026-06-20 19:04:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Value equal to position/value-equal-to-position.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class Solution {
+    public static ArrayList<Integer> valEqualToPos(int[] arr) {
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == i + 1) {
+                result.add(arr[i]);
+            }
+        }
+        
+        return result;
+    }
+}
