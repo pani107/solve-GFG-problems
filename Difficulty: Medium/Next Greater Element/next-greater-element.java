3d4b6ea50c2614b27e6bc15346e1eac14d5538fa
Pranav Wadkar <pranavwadkar2335@gmail.com>
2026-07-06 11:45:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        // code here
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> l = new ArrayList<>();
+        for (int i = 0; i < arr.length ; i++) {
+            l.add(-1);
+        }
+        for (int i = arr.length-1; i >=0 ; i--) {
+            while (!stack.isEmpty() && stack.peek()<= arr[i]){
+                stack.pop();
+            }
+            if(!stack.isEmpty()){
+                l.set(i, stack.peek());
+            }
+            stack.push(arr[i]);
+        }
+        return l;
+    }
+}
