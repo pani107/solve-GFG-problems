d8dacfd6a3631b7b9ea61321c9cf4e62437866c2
Pranav Wadkar <pranavwadkar2335@gmail.com>
2026-07-16 18:24:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Traversal Reverse/array-traversal-reverse.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static void arrayTraversalReverse(int[] arr, int n) {
+        // Code here
+        int i = 0;
+        int j = arr.length-1;
+        while(i < j){
+          int temp = arr[i];
+          arr[i] = arr[j];
+          arr[j] = temp;
+          i++;
+          j--;
+        }
+        for (int k = 0; k < n; k++) {
+            System.out.print(arr[k] + " ");
+        }
+    }
+}
