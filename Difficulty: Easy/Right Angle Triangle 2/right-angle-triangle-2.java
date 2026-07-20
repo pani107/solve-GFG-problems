8dd461a0b09745cfe649353c4be46bb8317b335b
Pranav Wadkar <pranavwadkar2335@gmail.com>
2026-07-20 18:11:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Right Angle Triangle 2/right-angle-triangle-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void printPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i || i == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+                if (j != i) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
