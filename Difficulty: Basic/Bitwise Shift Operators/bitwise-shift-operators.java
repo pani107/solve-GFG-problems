c94f73fc50b18a16e7f401b69ba2bd700d79b300
Pranav Wadkar <pranavwadkar2335@gmail.com>
2026-08-12 18:16:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Bitwise Shift Operators/bitwise-shift-operators.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int rightShift = a >> b;
+        int leftShift = a << b;
+
+        System.out.println(rightShift + " " + leftShift);
+    }
+}
